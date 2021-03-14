ec2fef9b33e5ad1b6b50d5e01660b2826896dcd4
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-03-14 17:21:54 +0530

    Update getEvent.js

--- Frontend/js/getEvent.js
+++ Frontend/js/getEvent.js
@@ -84,3 +84,14 @@ function switchTheme(event){
       lightMode();
   }
 }
+toggleSwitch.addEventListener('change',switchTheme);
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
+
