f7e9bbe3c5e93340629b2a6cd0766f64b0fe35b1
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-03-26 00:09:34 +0530

    Update getEvent.js

--- Frontend/js/getEvent.js
+++ Frontend/js/getEvent.js
@@ -243,3 +243,20 @@ window.addEventListener("DOMContentLoaded", function () {
     ajax(form.method, form.action, data, success, error);
   });
 });
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
